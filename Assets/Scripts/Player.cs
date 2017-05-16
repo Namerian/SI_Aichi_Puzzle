44b70a1cf0ff981ba408d1ba0a5bcfd16eef6e0e
Unknown <patrickmonville@hotmail.com>
2017-05-16 17:57:29 +0200

    implemented basic player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,15 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private string _name = "PlayerA";
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField]
+    private float _speed = 1.5f;
+
+    [SerializeField]
+    private Rigidbody _rigidbody;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float axisY = Input.GetAxis(_name + "_AxisY");
+        float axisX = Input.GetAxis(_name + "_AxisX");
+
+        _rigidbody.velocity = this.transform.forward * _speed;
+
+        if(axisY > 0)
+        {
+            this.transform.localEulerAngles = new Vector3(0, 0, 0);
+        }
+        else if(axisX > 0)
+        {
+            this.transform.localEulerAngles = new Vector3(0, 90, 0);
+        }
+        else if(axisY < 0)
+        {
+            this.transform.localEulerAngles = new Vector3(0, 180, 0);
+        }
+        else if(axisX < 0)
+        {
+            this.transform.localEulerAngles = new Vector3(0, -90, 0);
+        }
+    }
 }
