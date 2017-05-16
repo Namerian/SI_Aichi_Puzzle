67f006cc991fd056a7854e68254fd5955002e74f
Unknown <patrickmonville@hotmail.com>
2017-05-16 18:59:25 +0200

    implemented player collision with wall

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,11 +11,18 @@ public class Player : MonoBehaviour
     private float _speed = 1.5f;
 
     [SerializeField]
+    private float _stopTime = 0.8f;
+
+    [SerializeField]
     private Rigidbody _rigidbody;
+
+    private bool _stopped = false;
+    private Vector3 _lastRotation;
 
     // Use this for initialization
     void Start()
     {
+        _lastRotation = new Vector3(0, 90, 0);
     }
 
     // Update is called once per frame
@@ -24,23 +31,60 @@ public class Player : MonoBehaviour
         float axisY = Input.GetAxis(_name + "_AxisY");
         float axisX = Input.GetAxis(_name + "_AxisX");
 
-        _rigidbody.velocity = this.transform.forward * _speed;
+        if (_stopped)
+        {
+            _rigidbody.velocity = Vector3.zero;
+        }
+        else
+        {
+            _rigidbody.velocity = this.transform.forward * _speed;
 
-        if(axisY > 0)
-        {
-            this.transform.localEulerAngles = new Vector3(0, 0, 0);
+            Vector3 rot = Vector3.down;
+
+            if (axisY > 0)
+            {
+                rot = new Vector3(0, 0, 0);
+            }
+            else if (axisX > 0)
+            {
+                rot = new Vector3(0, 90, 0);
+            }
+            else if (axisY < 0)
+            {
+                rot = new Vector3(0, 180, 0);
+            }
+            else if (axisX < 0)
+            {
+                rot = new Vector3(0, 270, 0);
+            }
+
+            if (rot != Vector3.down && this.transform.localEulerAngles != rot)
+            {
+                _lastRotation = this.transform.localEulerAngles;
+                //Debug.Log(_lastRotation);
+                this.transform.localEulerAngles = rot;
+            }
         }
-        else if(axisX > 0)
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Wall"))
         {
-            this.transform.localEulerAngles = new Vector3(0, 90, 0);
+            //Debug.Log("ttttttttttttttttttt");
+
+            _stopped = true;
+
+            this.transform.localEulerAngles = _lastRotation;
+            _lastRotation = new Vector3(0, 90, 0);
+
+            Invoke("StartMoving", _stopTime);
         }
-        else if(axisY < 0)
-        {
-            this.transform.localEulerAngles = new Vector3(0, 180, 0);
-        }
-        else if(axisX < 0)
-        {
-            this.transform.localEulerAngles = new Vector3(0, -90, 0);
-        }
+    }
+
+    private void StartMoving()
+    {
+        //Debug.Log("rrrrr");
+        _stopped = false;
     }
 }
