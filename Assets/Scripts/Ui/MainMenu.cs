3aa6f8f7d8b095d765b5d150197eec82135732a2
Unknown <patrickmonville@hotmail.com>
2017-05-16 18:27:12 +0200

    created main menu

---
+++ Assets/Scripts/Ui/MainMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField]
+    private List<string> _levelNames;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnLevelButtonPressed(int id)
+    {
+        if(_levelNames.Count >= id)
+        {
+            SceneManager.LoadScene(_levelNames[id]);
+        }
+    }
+}
